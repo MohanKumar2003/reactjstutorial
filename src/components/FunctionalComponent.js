812fc53ba7013e0b81fdd9aa3cd5187b1571cb57
Mohan Kumar <mohankumar.parise5233@gmail.com>
2023-11-09 22:46:10 +0530

    FunctionalComponent is created

---
+++ src/components/FunctionalComponent.js
@@ -0,0 +1,15 @@
+
+import React from "react";
+
+const FunctionalComponent = () => {
+    let name = "Functional Component";
+    return (
+        <>
+            <center>
+                <h2>Welcome to {name}</h2>
+            </center>
+        </>
+    )
+}
+
+export default FunctionalComponent;
