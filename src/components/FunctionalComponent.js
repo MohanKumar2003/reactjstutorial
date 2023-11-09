d9ef2aae02cd82e36694b7ae342bcebb2339592a
Mohan Kumar <mohankumar.parise5233@gmail.com>
2023-11-09 23:47:13 +0530

    useState and props in Functional component

--- src/components/FunctionalComponent.js
+++ src/components/FunctionalComponent.js
@@ -1,15 +1,32 @@
 
-import React from "react";
+import React, { useState } from "react";
 
 const FunctionalComponent = () => {
+
+    const [data, setData] = useState({
+        name : "Madhu Mohan",
+        age : 22
+    })
     let name = "Functional Component";
     return (
         <>
             <center>
-                <h1>Welcome to {name}</h1>
+                <h1>Welcome to {data.name + " : " + data.age}</h1>
+                <Test name={data.name} age={data.age}/>
             </center>
         </>
     )
 }
 
 export default FunctionalComponent;
+
+// these is ome more function component
+const Test = (props) => {
+    return (
+        <>
+            <center>
+                <h1>Name : {props.name} and Age : {props.age}</h1>
+            </center>
+        </>
+    )
+}
