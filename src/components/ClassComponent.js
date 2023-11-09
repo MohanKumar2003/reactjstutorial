bcb51ebe94aa76e53bc22d3ff49673be624b5378
Mohan Kumar <mohankumar.parise5233@gmail.com>
2023-11-09 23:35:02 +0530

    state and props in classes

--- src/components/ClassComponent.js
+++ src/components/ClassComponent.js
@@ -3,16 +3,37 @@ import React, {Component} from "react";
 
 class ClassComponent extends React.Component {
 
+    // state
+    state = {
+        intro : "State and props in Class Component"
+    }
+
     render() {
         let name = "Class Based Component";
         return (
             <>
                 <center>
                     <h1>Welcome to {name}</h1>
+                    <h1>Welcome to {this.state.intro}</h1><hr/>
+                    <Test intro = {this.state.intro}/>
                 </center>
             </>
         )
     }
 }
 
+
 export default ClassComponent;
+
+// these one more componemt
+class Test extends React.Component {
+    render() {
+        return(
+            <>
+                <center>
+                    <h2>Intro : {this.props.intro}</h2>
+                </center>
+            </>
+        )
+    }
+}
